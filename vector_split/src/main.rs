ec033bc05a9788624bab710413e47b15855539aa
jamshaidtahiri <jamshaid.tahiri1@gmail.com>
2019-09-18 22:23:56 +0500

    first commit

--- vector_split/src/main.rs
+++ vector_split/src/main.rs
@@ -1,13 +1,29 @@
+// use std::io;
+// fn main() {
+    
+//     let mut input = String::new();
+//     io::stdin().read_line(&mut input);
+//     // let input = input.trim().parse().unwrap();
+    
+
+//     for i in input.split_whitespace(){
+//     println!("{}",i );
+//     }
+
+// }
+
 use std::io;
-fn main() {
+fn main(){
+    let mut int = String::new();
+    io::stdin().read_line(&mut int);
+    let int : i32 = int.trim().parse().unwrap();
     
-    let mut input = String::new();
+    let mut anyvec = vec![]; 
+    for i in 0..int{
+        let  mut input = String::new();
     io::stdin().read_line(&mut input);
-    // let input = input.trim().parse().unwrap();
-    
-
-    for i in input.split_whitespace(){
-    println!("{}",i );
+    let input : i32 = input.trim().parse().unwrap();
+    anyvec.push(input);
     }
-
+    println!("{:?}",anyvec);
 }
