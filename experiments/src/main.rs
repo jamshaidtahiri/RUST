d179254444bb11cb3b880f9ba1affe6b3505b0eb
jamshaidtahiri <jamshaid.tahiri1@gmail.com>
2019-09-19 23:31:36 +0500

    first commit

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -187,3 +187,30 @@
 //     }
 //     println!("{:?}",anyvec);
 // }
+
+
+use std::io;
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    // let mut input = input.trim();
+let mut anyvec = vec![];
+let mut vector : Vec<i32> = vec![];
+
+    for i in 0..input.len(){
+        let num = &input[i..i+1];
+        if num == " "{
+            let n = anyvec.join("");
+            let n:i32 = n.parse().unwrap();
+            vector.push(n);
+            for i in 0..anyvec.len(){
+                anyvec.pop();
+            }            
+
+        }else {
+            anyvec.push(num);
+        }
+        
+    }
+println!("{:?}",vector);
+}
