f822b9e737e8bb8ee3faa119077ee8ec5ff30c1d
jamshaidtahiri <jamshaid.tahiri1@gmail.com>
2019-09-20 22:27:41 +0500

    first commit

--- vector_split/src/main.rs
+++ vector_split/src/main.rs
@@ -12,18 +12,44 @@
 
 // }
 
-use std::io;
-fn main(){
-    let mut int = String::new();
-    io::stdin().read_line(&mut int);
-    let int : i32 = int.trim().parse().unwrap();
+// use std::io;
+// fn main(){
+//     let mut int = String::new();
+//     io::stdin().read_line(&mut int);
+//     let int : i32 = int.trim().parse().unwrap();
     
-    let mut anyvec = vec![]; 
-    for i in 0..int{
-        let  mut input = String::new();
+//     let mut anyvec = vec![]; 
+//     for i in 0..int{
+//         let  mut input = String::new();
+//     io::stdin().read_line(&mut input);
+//     let input : i32 = input.trim().parse().unwrap();
+//     anyvec.push(input);
+//     }
+//     println!("{:?}",anyvec);
+// }
+
+use std::io;
+fn main() {
+    let mut input = String::new();
     io::stdin().read_line(&mut input);
-    let input : i32 = input.trim().parse().unwrap();
-    anyvec.push(input);
+    // let mut input = input.trim();
+let mut anyvec = vec![];
+let mut vector : Vec<i32> = vec![];
+
+    for i in 0..input.len(){
+        let num = &input[i..i+1];
+        if num == " " || num == "\n"{
+            let n = anyvec.join("");
+            let n:i32 = n.parse().unwrap();
+            vector.push(n);
+            for _ in 0..anyvec.len(){
+                anyvec.pop();
+            }            
+
+        }else {
+            anyvec.push(num);
+        }
+                
     }
-    println!("{:?}",anyvec);
+println!("{:?}",vector);
 }
