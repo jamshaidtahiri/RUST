b254fef6053a5dc5fde3316ef86944370da0bff2
jamshaidtahiri <jamshaid.tahiri1@gmail.com>
2019-09-23 22:06:45 +0500

    first commit

--- module/src/main.rs
+++ module/src/main.rs
@@ -1,4 +1,15 @@
+// mod my_module;
+// fn main() {
+//     println!("{}",my_module::inner_module::print_hello());
+// }
+
+
+
 mod my_module;
+use my_module::Login;
 fn main() {
-    println!("{}",my_module::inner_module::print_hello());
+    let name = String::from("jamshaid");
+    let password = String::from("password");
+    let user1 = Login::login1(name,password);
+    print!("{:?}",user1);
 }
--- module/src/my_module.rs
+++ module/src/my_module.rs
@@ -1,5 +1,28 @@
-pub mod inner_module{
-    pub fn print_hello () -> String {
-        String::from("Hello from inner module under my module")
+
+
+
+#[derive(Debug)]
+pub struct Login {
+    name: String,
+    password: String, 
+}
+
+impl Login {
+    pub fn login1(name : String,password: String)->Login{
+        Login{
+            name,
+            password,
+        }
+
     }
 }
+
+
+
+// pub mod inner_module{
+//     pub fn print_hello () -> String {
+//         String::from("Hello from inner module under my module")
+//     }
+// }
+
+
